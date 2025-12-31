d595603b71f274855265311abe38dc2b41cef39c
AzadehMn <38600349+azadehmn@users.noreply.github.com>
2025-12-31 13:44:43 +0330

    chore: fix structure for icon implementation

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center p-10">
-    <IconTeacher class="text-gray-500 hover:text-primary" />
+    <Icon class="text-[#4567] hover:text-primary text-lg font-bold" name="teacher" />
 
-  
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 w-full max-w-4xl">
       <Header
         title="Card 1"
@@ -15,5 +14,4 @@
     </div>
   </div>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
