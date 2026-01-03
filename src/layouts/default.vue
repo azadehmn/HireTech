5553021bb8fb14420734c0ece59a2448eca3efd1
AzadehMn <38600349+azadehmn@users.noreply.github.com>
2026-01-03 22:33:19 +0330

    add layout default in project

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <!-- TopBar -->
+    <TopBarMain />
+    <!-- Main content -->
+    <main class="flex-1">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="flex flex-col items-center justify-center p-10 mt-[40px]">
-    <TopBarMain />
-
-    <!-- <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 w-full max-w-4xl">
+  <div class="flex flex-col items-center justify-center p-10 mt-[100px]">
+    
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 w-full max-w-4xl">
       <Header
         title="Card 1"
         description="Sample card using Tailwind + Design Tokens."
@@ -11,7 +10,7 @@
         title="Card 2"
         description="Ready to start building your project."
       />
-    </div> -->
+    </div>
   </div>
 </template>
 
